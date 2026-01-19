aca49aec04a0d6d0ff7e1c3f271fbace141c4abb
t79781459649 <t79781459649@sourcecraft.dev>
2026-01-19 05:35:54 +0000

    Initial commit

---
+++ gatsby-config.ts
@@ -0,0 +1,26 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `<<YOUR_SITE_TITLE>>`,
+    siteUrl: `https://<<YOUR_DOMAIN_NAME>>`,
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: "<<YOUR_DOMAIN_NAME>>",
+        region: "ru-central1",
+        protocol: "https",
+        hostname: "storage.yandexcloud.net",
+        customAwsEndpointHostname: 'storage.yandexcloud.net'
+      },
+    }
+  ],
+}
+
+export default config
